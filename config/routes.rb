21fca31bd4586ece74956a748a223eca03108afd
ArielFeingold <feingold.ariel@gmail.com>
2018-07-27 13:21:59 -0400

    2nd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :comments, only: [:create, :edit, :update, :new]
+  resources :events do
+    resources :locations, only: :show
+  end
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
+      resources :events
+      resources :locations
+    end
+  resources :rsvps, only: [:create, :destroy, :index]
+
+  resources :locations, only: :show
+
+  root 'static_pages#home'
+
+  get '/auth/facebook/callback' => 'sessions#create'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 end
